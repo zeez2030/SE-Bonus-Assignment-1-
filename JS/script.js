b29f58c99f1e6ac423fabcd0b0a6197e883d67ae
zeezgeeh <zeezrdq@gmail.com>
2018-12-12 13:26:19 +0200

    added html css and js files still unfinished

--- JS/script.js
+++ JS/script.js
@@ -9,18 +9,9 @@ const validate = function (title, sub, name, pub, year) {
         return false;
 
 };
-let x = 0;
-let submit2 = document.getElementById('zeez');
-submit2.addEventListener('click', function () {
-    let h1 = document.createElement('h1');
-    h1.textContent = "zeez";
-    document.getElementById('body').appendChild(h1)
-    if (x % 2 == 0)
-        document.getElementById('body').style.backgroundColor = 'white'
-    else            document.getElementById('body').style.backgroundColor = 'red'
-})
-submit.addEventListener('click', function () {
-
+submit.addEventListener('click', function (e) {
+        
+    
     let title = document.getElementById("title").value;
     let sub = document.getElementById("sub").value;
     let name = document.getElementById("auth").value;
@@ -29,8 +20,10 @@ submit.addEventListener('click', function () {
     let color = document.getElementById("color").value;
     if (validate(title, sub, name, pub, year)) {
 
-        window.location.href = ('./design.html');
-        document.getElementById("body").style.backgroundColor = color;
+       let firstpage= document.querySelector('.firstpage');
+        firstpage.style.visibility='hidden';
+       let body= document.querySelector('body');
+        body.style.background=color;
     } else
         alert("check inputs");
 
